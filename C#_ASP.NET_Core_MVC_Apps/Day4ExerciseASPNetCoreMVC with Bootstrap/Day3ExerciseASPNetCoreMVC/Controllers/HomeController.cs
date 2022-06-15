e94ae2551a01902f4113608eeae7504f0e7117f9
MaratNikitin <81712684+MaratNikitin@users.noreply.github.com>
2022-06-15 14:26:02 -0600

    ASP.NET Core MVC App Exercise With Bootstrap Was Added
    ASP.NET Core MVC App Exercise With Bootstrap Was Added

---
+++ C#_ASP.NET_Core_MVC_Apps/Day4ExerciseASPNetCoreMVC with Bootstrap/Day3ExerciseASPNetCoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Day3ExerciseASPNetCoreMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Day3ExerciseASPNetCoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.DiscountAmount = 0;
+            ViewBag.Total = 0;
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.DiscountAmount = model.CalculateDiscountAmount();
+                ViewBag.Total = model.CalculateTotal();
+            }
+            else
+            {
+                ViewBag.DiscountAmount = 0;
+                ViewBag.Total = 0;
+            }
+            return View(model);
+        }
+
+    }
+}
---
+++ C#_ASP.NET_Core_MVC_Apps/Day4ExerciseASPNetCoreMVC with Bootstrap/Day3ExerciseASPNetCoreMVC/Models/PriceQuotationModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Day3ExerciseASPNetCoreMVC.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage = "Please enter the Subtotal value.")]
+        [Range(0.000001, Double.MaxValue, ErrorMessage =
+               "Subtotal value must be greater than 0")]
+        public decimal? Subtotal { get; set; }
+
+        [Required(ErrorMessage = "Please enter the Discount Percent value.")]
+        [Range(0, 100, ErrorMessage =
+               "Discount percent value must be 0-100")]
+        public decimal? DiscountPct { get; set; }
+
+
+        public decimal? DiscountAmt { get; set; }
+
+
+        public decimal? Total { get; set; }
+
+
+        public decimal? CalculateDiscountAmount()
+        {
+            decimal? discountValue = Subtotal * DiscountPct / 100;
+            return discountValue;
+        }
+
+        public decimal? CalculateTotal()
+        {
+            decimal? totalValue = Subtotal * (1 - DiscountPct / 100);
+            return totalValue;
+        }
+    }
+}
