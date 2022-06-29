ff36b2e17fca310ecaf5515717e0ef05418333b6
MaratNikitin <81712684+MaratNikitin@users.noreply.github.com>
2022-06-29 07:53:54 -0600

    ASP.NET Core Blazor Server BudgetBookDemo App Added
    ASP.NET Core Blazor Server BudgetBookDemo App Added

---
+++ C#_ASP.NET_Core_Blazor_Server_Apps/BudgetBookDemo/BudgetBookDemo/Data/BudgetBookService.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * It's a simple .NET 6 Blazor Server application allowing to create a list of expenses.
+ * It's a part of self-learning Blazor Server.
+ * Author: Marat Nikitin
+ * When: June 2022
+ * This file contains async methods for working with user entries.
+ */
+
+namespace BudgetBookDemo.Data
+{
+    public class BudgetBookService
+    {
+        private static List<Entry> mockDb = new List<Entry>()
+        {
+            //new Entry(){Description="Test", Amount = 4.99m}
+        };
+
+        public async Task<bool> AddEntry(Entry newEntry)
+        {
+            mockDb.Add(newEntry);
+            return true;
+        }
+
+        public async Task<List<Entry>> GetAllEntries()
+        {
+            return mockDb;
+        }
+                
+    }
+}
---
+++ C#_ASP.NET_Core_Blazor_Server_Apps/BudgetBookDemo/BudgetBookDemo/Data/Entry.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * It's a simple .NET 6 Blazor Server application allowing to create a list of expenses.
+ * It's a part of self-learning Blazor Server.
+ * Author: Marat Nikitin
+ * When: June 2022
+ * This file contains the Entry class.
+ */
+
+namespace BudgetBookDemo.Data
+{
+    public class Entry
+    {
+        public decimal Amount { get; set; }
+        public string? Description { get; set; }
+    }
+}
