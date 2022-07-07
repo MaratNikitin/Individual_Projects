f839a084df247bc45d0bd92c6186ffde1b927b89
MaratNikitin <81712684+MaratNikitin@users.noreply.github.com>
2022-07-07 11:32:49 -0600

    Class Libraries for DB CRUD Operations Are Created
    Class Libraries for DB CRUD Operations Are Created.

---
+++ C#_ASP.NET_Core_Blazor_Server_Apps/Tangy/BusinessClassLibrary/Repository/CategoryRepository.cs
@@ -0,0 +1,83 @@
+﻿using AutoMapper;
+using BusinessClassLibrary.Repository.IRepository;
+using DataAccessClassLibrary;
+using DataAccessClassLibrary.Data;
+using ModelsClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessClassLibrary.Repository
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly IMapper _mapper;
+
+        public CategoryRepository(ApplicationDbContext db, IMapper mapper)
+        {
+            _db = db;
+            _mapper = mapper;
+        }
+
+        CategoryDTO ICategoryRepository.Create(CategoryDTO objDTO)
+        {
+            var obj = _mapper.Map<CategoryDTO, Category>(objDTO);
+            obj.CreatedDate = DateTime.Now;
+            var addedObj = _db.Categories.Add(obj);
+            _db.SaveChanges();
+
+            return _mapper.Map<Category, CategoryDTO>(addedObj.Entity);
+        }
+
+        int ICategoryRepository.Delete(int id)
+        {
+            var obj = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            if(obj!=null)
+            {
+                _db.Categories.Remove(obj);
+                return _db.SaveChanges();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        CategoryDTO ICategoryRepository.Get(int id)
+        {
+            var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            if (obj != null)
+            {
+                return _mapper.Map<Category, CategoryDTO>(obj);
+            }
+            else
+            {
+                return new CategoryDTO();
+            }
+        }
+
+        IEnumerable<CategoryDTO> ICategoryRepository.GetAll()
+        {
+            return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDTO>>(_db.Categories);
+        }
+
+        CategoryDTO ICategoryRepository.Update(CategoryDTO objDTO)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(u => u.Id == objDTO.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Name = objDTO.Name; // Name is the only column that can be updated
+                _db.Categories.Update(objFromDb); // changes here happen in Entity Framework Core only
+                _db.SaveChanges(); // saving changes in the DB itself
+                return _mapper.Map<Category, CategoryDTO>(objFromDb);
+            }
+            else
+            {
+                return objDTO;
+            }
+        }
+    }
+}
--- C#_ASP.NET_Core_Blazor_Server_Apps/Tangy/TangyWebServer/Program.cs
+++ C#_ASP.NET_Core_Blazor_Server_Apps/Tangy/TangyWebServer/Program.cs
@@ -1,5 +1,9 @@
+using BusinessClassLibrary.Repository;
+using BusinessClassLibrary.Repository.IRepository;
+using DataAccessClassLibrary.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using TangyWebServer.Data;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +12,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
 
