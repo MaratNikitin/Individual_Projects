fca173b6149711e2d54567b8eee9241f65c8c67f
MaratNikitin <81712684+MaratNikitin@users.noreply.github.com>
2022-07-07 16:08:31 -0600

    Create, Read, Update implemented
    Create, Read, Update implemented

--- C#_ASP.NET_Core_Blazor_Server_Apps/Tangy/ModelsClassLibrary/CategoryDTO.cs
+++ C#_ASP.NET_Core_Blazor_Server_Apps/Tangy/ModelsClassLibrary/CategoryDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace ModelsClassLibrary
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "The Name is required")]
         public string Name { get; set; }           
     }
 }
