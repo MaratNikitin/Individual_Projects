b7aa5f0e44a89667447251234ed088997fa8942e
MaratNikitin <81712684+MaratNikitin@users.noreply.github.com>
2022-07-13 19:06:49 -0600

    InAndOut MVC Core App Added.
    InAndOut MVC Core App Added.

---
+++ C#_ASP.NET_Core_MVC_Apps/InAndOut/InAndOut/Controllers/ExpenseController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InAndOut.Controllers
+{
+    public class ExpenseController : Controller
+    {
+
+        private readonly ApplicationDBContext db;
+
+        public ExpenseController(ApplicationDBContext _db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> objlist = db.Expenses;
+            return View(objlist);
+        }
+
+        // "GET" Create:
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // "POST" Create:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Expenses.Add(obj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // "GET" Delete:
+        public IActionResult Delete(int? ExpenseID)
+        {
+            if (ExpenseID == null || ExpenseID == 0)
+            {
+                return NotFound();
+            }
+            var obj = db.Expenses.Find(ExpenseID);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+
+        // "POST" Delete:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? ExpenseID)
+        {
+            var obj = db.Expenses.Find(ExpenseID);
+            if(obj == null) 
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                db.Expenses.Remove(obj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // "GET" Update:
+        public IActionResult Update(int? ExpenseID)
+        {
+            if (ExpenseID == null || ExpenseID == 0)
+            {
+                return NotFound();
+            }
+            var obj = db.Expenses.Find(ExpenseID);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // "POST" Update:
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Expense obj)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Expenses.Update(obj);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+    }
+}
---
+++ C#_ASP.NET_Core_MVC_Apps/InAndOut/InAndOut/Models/Expense.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InAndOut.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int ExpenseId { get; set; }
+
+        [Required] // validating input data:
+        [Column(TypeName = "varchar(50)")]
+        [DisplayName("Expense Name")]
+        public string ExpenseName { get; set; }
+
+        [Required] // validating input data
+        [Column(TypeName = "decimal(18,2)")]
+        [DisplayName("Expense Amount, $")]
+        [Range(0, 1000000000, ErrorMessage = "Expense amount must be positive")] // validating input data
+        public decimal ExpenseAmount { get; set; }
+    }
+}
