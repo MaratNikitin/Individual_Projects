dca2e33819d02d4ba829c97dbfc575e8010ab03c
Marat Nikitin <maratn@graham.ca>
2022-07-20 08:15:40 -0600

    Extra comments added
    Extra comments added

--- C#_ASP.NET_Core_MVC_Apps/InAndOut/InAndOut/Data/ApplicationDBContext.cs
+++ C#_ASP.NET_Core_MVC_Apps/InAndOut/InAndOut/Data/ApplicationDBContext.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/*
+ * This ASP.NET Core MVC app creates two pages used for tracking borrowed items and a list of expences allowing full range of CRUD
+ *      operations using a created (Code First) SQL Server database. This app is inspired by a Udemy ASP.NET course.
+ * Author: Marat Nikitin.
+ * When: January 2022.
+ * This file is used to access DB data.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
