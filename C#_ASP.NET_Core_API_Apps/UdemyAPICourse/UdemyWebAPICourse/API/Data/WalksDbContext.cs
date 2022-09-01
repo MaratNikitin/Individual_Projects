3e38c06a55ed95f0b00172f61e00ff5ccf704b0b
Marat Nikitin <maratn@graham.ca>
2022-09-01 09:26:07 -0600

    Created classes for DB (Code First).

---
+++ C#_ASP.NET_Core_API_Apps/UdemyAPICourse/UdemyWebAPICourse/API/Data/WalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using API.Models.Domain
+
+namespace API.Data
+{
+    public class WalksDbContext: DbContext
+    {
+        public WalksDbContext(DbContextOptions<WalksDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulties { get; set; }
+    }
+}
