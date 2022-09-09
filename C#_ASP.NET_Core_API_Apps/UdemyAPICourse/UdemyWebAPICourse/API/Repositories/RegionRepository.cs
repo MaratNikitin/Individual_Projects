22aa2c85a69f0348164efe146312c9c0f54adba3
Marat Nikitin <maratn@graham.ca>
2022-09-09 14:41:24 -0600

    GetAllRegions async method is introduced with DTO.

---
+++ C#_ASP.NET_Core_API_Apps/UdemyAPICourse/UdemyWebAPICourse/API/Repositories/RegionRepository.cs
@@ -0,0 +1,21 @@
+﻿using API.Data;
+using API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly WalksDbContext walksDbContext;
+
+        public RegionRepository(WalksDbContext walksDbContext)
+        {
+            this.walksDbContext = walksDbContext;
+        }
+        
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await walksDbContext.Regions.ToListAsync();
+        }
+    }
+}
