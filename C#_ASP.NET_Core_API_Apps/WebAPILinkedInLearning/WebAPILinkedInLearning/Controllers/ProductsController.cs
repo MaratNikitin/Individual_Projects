1f338765819bdac1642a7de784188bc938ec2900
Marat Nikitin <maratn@graham.ca>
2022-12-11 10:11:23 -0700

    Created a complete REST API based on LinkedIn Web API course.

---
+++ C#_ASP.NET_Core_API_Apps/WebAPILinkedInLearning/WebAPILinkedInLearning/Controllers/ProductsController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using WebAPILinkedInLearning.Models;
+
+namespace WebAPILinkedInLearning.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopDbContext _context;
+        public ProductsController (ShopDbContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        [Route("/alt")]
+        public async Task<ActionResult<IEnumerable<Product>>> GetAllProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public async Task<IEnumerable<Product>> GetAllProductsAlt()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        [HttpGet]
+        [Route("/{id}")]
+        public async Task<ActionResult> GetProduct(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product == null) { 
+                return NotFound(); 
+            }
+            return Ok(product);  
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                "GetProduct",
+                new {id = product.Id},
+                product);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateProduct(
+            int id, [FromBody] Product product)
+        {
+            if (id != product.Id) { 
+                return BadRequest();
+            }
+            try
+            {
+                _context.Entry(product).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Products.Any(p => p.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProduct(int id)
+        {
+            var product = _context.Products.FirstOrDefault(p => p.Id == id);
+            if(product == null)
+            {
+                return NotFound(id);
+            }
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+
+        [HttpPost]
+        [Route("Delete")]
+        public async Task<ActionResult> DeleteMultopleProducts([FromQuery] int[] ids)
+        {
+            var products = new List<Product>();
+            foreach (var id in ids)
+            {
+                var product = await _context.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if (product == null)
+                {
+                    return NotFound(id);
+                }
+                products.Add(product);
+
+            }
+            _context.Products.RemoveRange(products);
+            await _context.SaveChangesAsync();
+            return Ok(products);
+        }
+
+    }
+}
