a046b7604a9e991114461a2dc789fb252e977d03
Marat Nikitin <maratn@graham.ca>
2022-12-20 18:54:15 -0700

    Enabled CORS from Program.cs

--- C#_ASP.NET_Core_API_Apps/WebAPILinkedInLearning/WebAPILinkedInLearning/Program.cs
+++ C#_ASP.NET_Core_API_Apps/WebAPILinkedInLearning/WebAPILinkedInLearning/Program.cs
@@ -15,6 +15,15 @@ builder.Services.AddDbContext<ShopDbContext>(options =>
     options.UseInMemoryDatabase("Shop");
 });
 
+// Allow CORS.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder => {
+        builder.AllowAnyOrigin();
+        builder.WithOrigins("https://localhost:7257").WithHeaders("X-API-Version");
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -24,13 +33,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 } else
 {
-    // Enforcing use of HTTPS instead of HTTP for Production environment
+    // Enforce use of HTTPS only instead of HTTP for Production environment.
     app.UseHsts(); 
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Allow using CORS activated:
+app.UseCors();
 
 app.MapControllers();
 
