fb15662f03dd94d0a4540ece7715fafc887f31a9
Marat Nikitin <maratn@graham.ca>
2023-03-16 17:48:55 -0600

    API for AssignmentV2 is created.

---
+++ C#_ASP.NET_Core_MVC_Apps/Udemy2023/AssignmentV2/API/Models/Assignment2023Context.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+public partial class Assignment2023Context : DbContext
+{
+    public Assignment2023Context()
+    {
+    }
+
+    public Assignment2023Context(DbContextOptions<Assignment2023Context> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Item> Items { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Item>(entity =>
+        {
+            entity.HasOne(d => d.Category).WithMany(p => p.Items)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Items_Categories");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ C#_ASP.NET_Core_MVC_Apps/Udemy2023/AssignmentV2/API/Models/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace API.Models;
+
+public partial class Item
+{
+    [Key]
+    [Column("ItemID")]
+    public int ItemId { get; set; }
+
+    [Column("CategoryID")]
+    public int CategoryId { get; set; }
+
+    [StringLength(100)]
+    public string ItemName { get; set; } = null!;
+
+    [Column(TypeName = "money")]
+    public decimal ItemValue { get; set; }
+
+    [ForeignKey("CategoryId")]
+    [InverseProperty("Items")]
+    public virtual Category Category { get; set; } = null!;
+}
