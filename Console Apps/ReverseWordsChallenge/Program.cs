02e77ae198ac96cd28f6ec23dce07c6ee6bf633c
Marat Nikitin <maratn@graham.ca>
2023-07-13 08:35:26 -0600

    ReverseWordsChallenge is completed.

---
+++ Console Apps/ReverseWordsChallenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+static string ReverseAWord(string input)
+{
+    if (String.IsNullOrEmpty(input))
+    {
+        return "";
+    }
+
+    StringBuilder reversed = new StringBuilder();
+    for (int i = input.Length - 1; i >= 0; i--)
+    {
+        reversed.Append(input[i]);
+    }
+
+    return reversed.ToString();
+}
+
+static string ReverseEachWordInASentence(string input)
+{
+    string [] words = input.Split(" ");
+    StringBuilder sentenceWithReversedWords = new();
+    foreach (string word in words)
+    {
+        sentenceWithReversedWords.Append(ReverseAWord(word) + " ");
+    }
+    return sentenceWithReversedWords.ToString();
+}
+
+string testSentence = "Today is July 13th 2023";
+Console.WriteLine($"Original sentence: {testSentence}");
+Console.WriteLine($"Sentence with each word reversed: {ReverseEachWordInASentence(testSentence)}");
