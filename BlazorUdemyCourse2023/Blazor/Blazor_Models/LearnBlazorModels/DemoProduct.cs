e3ae4c62bc7a9f1912b28324e025dc105d8bba66
Marat Nikitin <maratn@graham.ca>
2023-10-23 16:44:54 -0600

    First 20 videos are complete.

---
+++ BlazorUdemyCourse2023/Blazor/Blazor_Models/LearnBlazorModels/DemoProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_Models.LearnBlazorModels
+{
+    public class DemoProduct
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public bool IsActive { get; set; }
+        public IEnumerable<DemoProductProperties> ProductProperties { get; set; }
+    }
+}
---
+++ BlazorUdemyCourse2023/Blazor/Blazor_Models/LearnBlazorModels/DemoProductProperties.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor_Models.LearnBlazorModels
+{
+    public class DemoProductProperties
+    {
+        public int Id { get; set; }
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
