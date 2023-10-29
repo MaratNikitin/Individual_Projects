2058f92bdc5e8bf4736906576915e993160a794e
Marat Nikitin <maratn@graham.ca>
2023-10-29 09:07:26 -0600

    Lesson 26 completed.

--- BlazorUdemyCourse2023/Blazor/Blazor_Models/LearnBlazorModels/DemoProduct.cs
+++ BlazorUdemyCourse2023/Blazor/Blazor_Models/LearnBlazorModels/DemoProduct.cs
@@ -12,6 +12,6 @@ namespace Blazor_Models.LearnBlazorModels
         public string Name { get; set; }
         public double Price { get; set; }
         public bool IsActive { get; set; }
-        public IEnumerable<DemoProductProperties> ProductProperties { get; set; }
+        public List<DemoProductProperties> ProductProperties { get; set; }
     }
 }
