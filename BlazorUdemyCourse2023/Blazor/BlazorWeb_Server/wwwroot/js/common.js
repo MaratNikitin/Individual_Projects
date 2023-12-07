395e44e8bbc69705764180772a5cacbf8dde9214
Marat Nikitin <maratn@graham.ca>
2023-12-06 17:21:22 -0700

    Lesson 56 completed.

---
+++ BlazorUdemyCourse2023/Blazor/BlazorWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,29 @@
+﻿window.ShowToastr = (type, message) => {
+
+    if (type === "success") {
+        toastr.success(message, "Success!", {timeOut: 7000})
+    }
+
+    if (type === "error") {
+        toastr.error(message, "Error!", { timeOut: 7000 })
+    }
+}
+
+window.ShowSweetAlert = (type, message) => {
+
+    if (type === "success") {
+        Swal.fire(
+            'Success Notification',
+            message,
+            'success'
+        )
+    }
+
+    if (type === "error") {
+        Swal.fire(
+            'Error Notification',
+            message,
+            'error'
+        )
+    }
+}
