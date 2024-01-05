f61f0cffd5fa0a42e864fe32282d909abb198ab1
Marat Nikitin <maratn@graham.ca>
2024-01-05 16:33:17 -0700

    Single Counter with Timer .NET 8 console app is created.

---
+++ Console Apps/SimpleCounterWithTimer/SimpleCounterWithTimer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+
+int inputNumber = 123456789;
+var stopWatch = new Stopwatch();
+
+Console.WriteLine($"Started execution ...");
+
+stopWatch.Start();
+
+BigInteger cumulativeSum = 0;
+
+for (int i = 0; i < inputNumber+1; i++)
+{
+    cumulativeSum += i;
+}
+
+stopWatch.Stop();
+
+Console.WriteLine($"The execution time: {stopWatch.ElapsedMilliseconds} ms.");
+Console.WriteLine($"The cumulative sum of all natural numbers from 0 to {inputNumber} (inclusive) is {cumulativeSum}.");
+Console.WriteLine($"Mission accomplished!");
