6e2a891c734c0287cd217d2456bf5a8bab827940
Marat Nikitin <maratn@graham.ca>
2024-04-03 18:58:27 -0600

    Completed the three letters challenge.

---
+++ Console Apps/CodilityChallengeThreeLetters/ThreeLetters/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Text;
+
+string GetABCombinations(int A, int B)
+{
+    StringBuilder stringBuilder = new StringBuilder();
+    int remainingAs = A;
+    int remainingBs = B;
+    int consecutiveAsCounter = 0;
+    int consecutiveBsCounter = 0;
+
+    for (int i = 0; i < A+B; i++)
+    {
+        if(remainingAs > remainingBs && consecutiveAsCounter < 2)
+        {
+            stringBuilder.Append("a");
+            remainingAs--;
+            consecutiveAsCounter++;
+            consecutiveBsCounter = 0;
+            continue;
+        }
+
+        if (remainingBs > remainingAs && consecutiveBsCounter < 2)
+        {
+            stringBuilder.Append("b");
+            remainingBs--;
+            consecutiveBsCounter++;
+            consecutiveAsCounter = 0;
+            continue;
+        }
+
+        if (consecutiveBsCounter == 2)
+        {
+            stringBuilder.Append("a");
+            remainingAs--;
+            consecutiveAsCounter++;
+            consecutiveBsCounter = 0;
+            continue;
+        }
+
+        if (consecutiveAsCounter == 2)
+        {
+            stringBuilder.Append("b");
+            remainingBs--;
+            consecutiveBsCounter++;
+            consecutiveAsCounter = 0;
+            continue;
+        }
+
+        if (consecutiveAsCounter >= consecutiveBsCounter)
+        {
+            stringBuilder.Append("a");
+            remainingAs--;
+            consecutiveAsCounter++;
+            consecutiveBsCounter = 0;
+            continue;
+        }
+        else
+        {
+            stringBuilder.Append("b");
+            remainingBs--;
+            consecutiveBsCounter++;
+            consecutiveAsCounter = 0;
+            continue;
+        }
+    }
+
+    return stringBuilder.ToString();
+}
+
+int numberOfAs = 5;
+int numberOfBs = 3;
+
+string result = GetABCombinations(numberOfAs, numberOfBs);
+Console.WriteLine($"The resulting string for A = {numberOfAs} and B = {numberOfBs}: {result}");
