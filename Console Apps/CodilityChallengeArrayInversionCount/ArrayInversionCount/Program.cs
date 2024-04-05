2aea9527a5a1ac706f74f0a1bad0f8949537114c
Marat Nikitin <maratn@graham.ca>
2024-04-04 20:10:54 -0600

    Completed the ArrayInversionCount challenge.

---
+++ Console Apps/CodilityChallengeArrayInversionCount/ArrayInversionCount/Program.cs
@@ -0,0 +1,25 @@
+﻿const int N = 6;
+int[] inputArray = new int[N] { -1, 6, 3, 4, 7, 4 };
+
+//const int N = 3;
+//int[] inputArray = new int[N] { 3,4,1 };
+
+int CalculateNumberOfIntegers(int[] A)
+{
+    int inversionsCount = 0;
+
+    for (int i = 0; i < A.Length; i++) 
+    {
+        for (int j = i+1; j < A.Length; j++) 
+        {
+            if (A[i] > A[j])
+            {
+                inversionsCount++;
+            }
+        }
+    }
+
+    return inversionsCount;
+}
+
+Console.WriteLine($"The number of inversions is {CalculateNumberOfIntegers(inputArray)}.");
