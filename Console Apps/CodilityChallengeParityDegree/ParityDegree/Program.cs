22ca14f77e331b0fadc9d016f67d25de1a8ff06e
Marat Nikitin <maratn@graham.ca>
2024-04-05 18:45:15 -0600

    Codility's ParityDegree challenge is resolved.

---
+++ Console Apps/CodilityChallengeParityDegree/ParityDegree/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+int GetHighestPowerOfTwoDivider(int N)
+{
+    var power = 0;
+    int modulo = 0;
+
+    while(modulo == 0)
+    {
+        power++;
+        modulo = N % (int)Math.Pow(2, power);
+    }
+
+    return --power;
+}
+
+var inputNumber = 1;
+Console.WriteLine($"For N = {inputNumber} the highest power of 2 that divides N is {GetHighestPowerOfTwoDivider(inputNumber)}");
